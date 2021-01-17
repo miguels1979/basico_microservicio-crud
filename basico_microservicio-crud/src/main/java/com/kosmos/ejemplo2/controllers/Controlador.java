7a9e2850df9d2b03cf7e8efd4181735f0142bff0
miguel <gravitones@hotmail.com>
2021-01-16 21:16:57 -0600

    DeleteMapping

--- basico_microservicio-crud/src/main/java/com/kosmos/ejemplo2/controllers/Controlador.java
+++ basico_microservicio-crud/src/main/java/com/kosmos/ejemplo2/controllers/Controlador.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,5 +51,11 @@ public class Controlador {
 		return aux;
 	}
 	//Prueba, Prueba4, Prueba5, Prueba6
+	
+	@DeleteMapping(value="curso/{name}")
+	public void eliminarCurso(@PathVariable("name") String nombre) {
+		cursos.removeIf(c-> c.getNombre().equals(nombre));
+	}
+	
 
 }
