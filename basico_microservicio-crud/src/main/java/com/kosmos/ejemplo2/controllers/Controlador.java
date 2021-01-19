e0f509b92dcc22491c73976328d71e922ea86473
miguel <gravitones@hotmail.com>
2021-01-18 21:14:12 -0600

    PostMapping, y PutMapping

--- basico_microservicio-crud/src/main/java/com/kosmos/ejemplo2/controllers/Controlador.java
+++ basico_microservicio-crud/src/main/java/com/kosmos/ejemplo2/controllers/Controlador.java
@@ -9,6 +9,9 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kosmos.ejemplo2.model.Curso;
@@ -56,6 +59,31 @@ public class Controlador {
 	public void eliminarCurso(@PathVariable("name") String nombre) {
 		cursos.removeIf(c-> c.getNombre().equals(nombre));
 	}
+	//En el consumes defenimos el tipo de dato que va a recibir y en el produces devolvemos el tipo de dato procesado
+	
+	@PostMapping(value="curso" , consumes= MediaType.APPLICATION_JSON_VALUE, 
+					produces= MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> altaCurso(@RequestBody Curso curso){
+		cursos.add(curso);
+		return cursos;
+	}
+	
+	@PutMapping(value="curso" , consumes= MediaType.APPLICATION_JSON_VALUE, 
+			produces= MediaType.APPLICATION_JSON_VALUE)
+	public List<Curso> actualizaCurso(@RequestBody Curso curso){
+		
+		// se realiza la modificación con un for estandar porque 
+		//hay que modificar la posición en donde encontremos un curso que se llame
+		//con el mismo nombre que viene en el cuerpo
+		for(int i = 0 ; i<cursos.size(); i++) {
+			if(cursos.get(i).getNombre().equalsIgnoreCase(curso.getNombre())) {
+				cursos.set(i, curso);
+			}
+		}
+		
+		return cursos;
+	}
+	
 	
 
 }
